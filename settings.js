10750b5db73afd2400578b617cbaf7b8eaac7bd6
EpahBot <101261029+EpahBot@users.noreply.github.com>
2022-07-01 13:48:02 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,8 +13,8 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
-global.autobio = true 
+global.autoread = true // auto read pesan / message
+global.autobio = false
 
 //language
 //Available in indonesia & english
@@ -23,23 +23,23 @@ global.language = english  //change indonesia to english if you don't understand
 
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.botname = "BebanBot天" //namabot kalian
+global.ownername= "Khairil春" //nama kalian
+global.myweb ="https://wa.me/60109102984" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/Khairil🅥" //bebas asal jan hapus
+global.github = "https://github.com/KhairilOFC" //bebas
+global.email = "airilkhairil88@gmail.com" //bebas
+global.region = "Malaysia" //bebas
+global.timezone = 'Asia/Kuala Lumpur' //  timezone wib
+global.owner = ["60109102984","601124024797"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "60109102984" // nomor wa kalian
+global.ownernomerr = "+60109102984" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© BebanBot天-' //sticker wm ubah
+global.author = 'By Khairil春' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
